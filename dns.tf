a822646d026b758d28e799cc3a46e8556b6d691c
Nathanael Liechti <technat@technat.ch>
2026-02-14 12:40:15 +0100

    chore: use new resource to configure magic dns

--- dns.tf
+++ dns.tf
@@ -1,3 +1,9 @@
-resource "tailscale_dns_preferences" "preferences" {
+resource "tailscale_dns_configuration" "configuration" {
   magic_dns = true
+}
+
+locals {
+  # generated in the console after magic_dns was enabled
+  # cannot be changed again once magic https is enabled (see settings.tf)
+  domain_name = "lamb-exponential.ts.net"
 }
