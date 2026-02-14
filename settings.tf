101c55b3342716926aa127f7aaaf17edbfe9fdc7
Nathanael Liechti <technat@technat.ch>
2026-02-14 10:05:28 +0100

    chore: configure ts settings

---
+++ settings.tf
@@ -0,0 +1,11 @@
+resource "tailscale_tailnet_settings" "settings" {
+  acls_externally_managed_on                  = true
+  acls_external_link                          = "https://github.com/the-technat/tailnet"
+  devices_approval_on                         = true
+  devices_auto_updates_on                     = true
+  devices_key_duration_days                   = 30
+  users_approval_on                           = true
+  users_role_allowed_to_join_external_tailnet = "member"
+  posture_identity_collection_on              = true
+  https_enabled                               = true
+}
